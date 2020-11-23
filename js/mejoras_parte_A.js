23e92a04910770efa0f4146972ce17361b09f88a
Alejandro Serrano Loredo <alejandroserranol@hotmail.com>
2020-11-23 12:55:14 +0100

    En proceso de hacer cookies

--- js/mejoras_parte_A.js
+++ js/mejoras_parte_A.js
@@ -19,9 +19,12 @@ function eligeNumVerbos(_numVerbos) {
         icono = '<i class="fas fa-gem text-info"></i>';
     }
 
+    $("#nivel").text("Nivel: " + _numVerbos);
+
     $("#btnsEligeNumVerbos").addClass("d-none");
     $("#labelNumVerbos").addClass("d-none");
 
+    $("#nivel").removeClass("d-none");
     $("#marcador").removeClass("d-none");
     $("#castellano").removeClass("d-none");
     $("#verbos").removeClass("d-none");
@@ -29,20 +32,25 @@ function eligeNumVerbos(_numVerbos) {
 }
 
 function cambiaNivel() {
-    if (jugando == true) {
         jugando = false;
         marcador = 0
 
         $("#marcador").text("");
+        $("#nivel").text("");
 
         $("#btnsEligeNumVerbos").removeClass("d-none");
         $("#labelNumVerbos").removeClass("d-none");
 
+        $("#nivel").addClass("d-none");
         $("#marcador").addClass("d-none");
         $("#castellano").addClass("d-none");
         $("#verbos").addClass("d-none");
         $("#botonResultado").addClass("d-none");
-    }
+
+        eligeVerbo();
+        $("#botonResultado").removeClass("btn-success");
+        $("#botonResultado").removeClass("btn-danger");
+        $("#botonResultado").addClass("btn-dark");
 }
 
 
